ecb13a18bb62e1add2591e4affe4718490b22e5c
Eli Moshe <moshebogo@gmail.com>
2023-03-18 23:14:34 -0400

    attempt 1 at db

--- code-challenge/app/models.py
+++ code-challenge/app/models.py
@@ -6,13 +6,23 @@ class Restaurant(db.Model):
     __tablename__ = 'restaurants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    address = db.Column(db.String)
 
 class Pizza(db.Model):
     __tablename__ = 'pizzas'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    ingredients = db.Column(db.String)
+    created_at = db.Column(db.DateTime)
+    updated_at = db.Column(db.DateTime)
 
 class RestaurantPizza(db.Model):
     __tablename__ = 'restaurant_pizzas'
 
     id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Integer)
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'))
+    Restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
+
