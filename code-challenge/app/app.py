b887019f63538cf51726fa7b27862e0a9995ea75
Eli Moshe <moshebogo@gmail.com>
2023-03-19 04:57:30 -0400

    85 % there!

--- code-challenge/app/app.py
+++ code-challenge/app/app.py
@@ -16,23 +16,78 @@ db.init_app(app)
 
 @app.route('/')
 def home():
-    return ''
+    pass
 
-@app.route('/restaurants')
+@app.route('/restaurants', methods= ['GET', 'POST'])
 def restaurants():
-    return ''
+    if request.method == 'GET':
+        restaraunts = Restaurant.query.all()
+        restaurants_to_dict = [restaurant.to_dict() for restaurant in restaraunts] 
+        return make_response(jsonify(restaurants_to_dict), 200)
+        
+    elif request.method =='POST':
+        data = request.get_json()
+        new_restaraunt = Restaurant()
+        for key in data:
+            setattr(new_restaraunt, key, data[key])
+        db.session.add(new_restaraunt)
+        db.session.commit()
+        return make_response(jsonify(new_restaraunt.to_dict()), 201)
 
-@app.route('/restaurants/<int:id>')
+@app.route('/restaurants/<int:id>', methods = ['GET', 'PATCH', 'DELETE'])
 def restaurant_by_id(id):
-    return ''
+    restaurant = Restaurant.query.get(id)
+    if not restaurant:
+        return make_response(jsonify({'error': 'that restaraunt does not exist'}), 400)
+    
+    elif request.method =='GET':    
+        return make_response(jsonify(restaurant.to_dict()), 200)
+    
+    elif request.method == 'PATCH':
+        data = request.get_json()
+        for key in data:
+            setattr(restaurant, key, data[key])
+        db.session.add(restaurant)
+        db.session.commit() 
+        return make_response(jsonify(restaurant.to_dict()), 200)  
 
-@app.route('/pizzas')
+    elif request.method == 'DELETE':
+        db.session.delete(restaurant)
+        db.session.commit()
+        return make_response(jsonify({'status': 'DELETE successful'}), 200)
+        
+        
+
+@app.route('/pizzas', methods = ['GET', 'POST'])
 def pizzas():
-    return ''
-
-@app.route('/restaurant_pizzas')
+    if request.method == 'GET':
+        pizzas = Pizza.query.all()
+        pizzas_to_dict = [pizza.to_dict() for pizza in pizzas]
+        return make_response(jsonify(pizzas_to_dict), 200)
+    
+    elif request.method == 'POST':
+        data = request.get_json()
+        new_pizza = Pizza()
+        for key in data:
+            setattr(new_pizza, key, data[key])
+        db.session.add(new_pizza)   
+        db.session.commit()
+        return make_response(jsonify(new_pizza.to_dict()), 201) 
+    
+@app.route('/restaurant_pizzas', methods = ['GET', 'POST'])
 def resaurant_pizzas():
-    return ''
+    if request.method == 'POST':
+        data = request.get_json()
+        new_rest_pizza = RestaurantPizza()
+        for key in data:
+            setattr(new_rest_pizza, key, data[key])
+        # checks if that pizza_id exists. If it does, adds it. otherwise, sends error.
+        pizza_exists = Pizza.query.get(new_rest_pizza.pizza_id)
+        if not pizza_exists:
+            return make_response(jsonify({'error': 'that pizza_iddoes not exist, not added to db'}))
+        db.session.add(new_rest_pizza)
+        db.session.commit()
+        return make_response(jsonify(pizza_exists.to_dist()), 201) 
 
 
 if __name__ == '__main__':
--- code-challenge/app/models.py
+++ code-challenge/app/models.py
@@ -8,6 +8,12 @@ class Restaurant(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     address = db.Column(db.String)
+    restaurant_pizzas = db.relationship('RestaurantPizza', backref='restaurant')
+
+    def to_dict(self):
+        return {'Id': self.id,
+                'name': self.name,
+                'address': self.address}
 
 class Pizza(db.Model):
     __tablename__ = 'pizzas'
@@ -17,12 +23,28 @@ class Pizza(db.Model):
     ingredients = db.Column(db.String)
     created_at = db.Column(db.DateTime)
     updated_at = db.Column(db.DateTime)
+    restaurant_pizzas = db.relationship('RestaurantPizza', backref='pizza')
+
+    def to_dict(self):
+        return {'Id': self.id,
+                'name': self.name,
+                'ingredients': self.ingredients,
+                'created_at': self.created_at,
+                'updated_at': self.updated_at}
 
 class RestaurantPizza(db.Model):
     __tablename__ = 'restaurant_pizzas'
 
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer)
-    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'))
-    Restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
+    created_at = db.Column(db.DateTime)
+    updated_at = db.Column(db.DateTime)
 
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
+    pizza_id =      db.Column(db.Integer, db.ForeignKey('pizzas.id'))
+
+    def to_dict(self):
+        return {'Id': self.id,
+                'price': self.price,
+                'created_at': self.created_at,
+                'updated_at': self.updated_at}
